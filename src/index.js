4efe64e4b94cd7884a87a1e0c881777ef35490b9
koechkevin <koechkevin92@gmail.com>
2019-07-06 12:01:17 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import app from './app';
-import dotenv from 'dotenv';
+import env from 'dotenv';
 
-dotenv.config();
+env.config();
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
